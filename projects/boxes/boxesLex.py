d236ba57e6cae26a8c6a0b39b87b63f2bd36b9a7
David Lopez <dnlm92@me.com>
2014-03-08 21:21:04 -0600

    [MODIFY] Error de lexico mas especifico

--- projects/boxes/boxesLex.py
+++ projects/boxes/boxesLex.py
@@ -128,7 +128,7 @@ t_ignore  = ' \t'
 
 # Error handling rule
 def t_error(t):
-    print "Illegal character '%s'" % t.value[0]
+    print "BoxesLexerError: Illegal character '%s'" % t.value[0]
     t.lexer.skip(1)
     exit(1)
 
